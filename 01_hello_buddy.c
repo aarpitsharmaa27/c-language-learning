e4c45d47e94c1f209d2cf2629e967a1bc2ededf9
Arpit <aarpitsharmaa27@gmail.com>
2025-07-30 21:41:46 +0530

    Add files via upload
    First and most Basic C program.

---
+++ 01_hello_buddy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+    printf("Hello !! Bro\n");       // we use /n for new line
+    /*
+    For
+    multi line comments
+    we use 
+    this */
+    
+    printf("I like PIzza");
+
+    
+    return 0;    
+}
