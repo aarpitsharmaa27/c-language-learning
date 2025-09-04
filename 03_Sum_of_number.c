1d5371bf3066d809b35eab314d463ff8f9f16a63
Arpit <aarpitsharmaa27@gmail.com>
2025-09-04 22:39:18 +0530

    Update 03_Sum_of_number.c

--- 03_Sum_of_number.c
+++ 03_Sum_of_number.c
@@ -1,13 +1,31 @@
 #include<stdio.h>
 int main(){
     int a, b ;
-    printf("enter a");
+    printf("enter a :");
     scanf("%d", &a);
 
-    printf("enter b");
+    printf("enter b :");
     scanf("%d",&b);
-    
-    
 
+    int sum =  a + b;
+    printf("Sum is %d",sum);
     return 0;
 }
+
+
+// Another Method 
+
+#include<stdio.h>
+int main(){
+    int a, b ;
+    printf("enter a :");
+    scanf("%d", &a);
+
+    printf("enter b :");
+    scanf("%d",&b);
+
+    
+    printf("Sum is %d",a + b);
+    return 0;
+}
+
